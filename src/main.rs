34f38bd185a92062a8c243fdef5d89763e5acf76
gorilskij <43991400@users.noreply.github.com>
2020-04-17 08:24:07 +0200

    implement basic nn

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![feature(stmt_expr_attributes)] // for fine-grained rustfmt control
 
 use crate::game::visual::VisGame;
+use crate::neural_network::NN;
 
 pub mod game;
 mod neural_network;
 mod support;
 
 fn main() {
-    VisGame::run();
+    // VisGame::run();
+    let nn = NN::new(&[4, 10, 6]);
+    let out = nn.apply(&[1., 2., 3., 4.]);
+    println!("{:?}", out);
 }
--- src/neural_network.rs
+++ src/neural_network.rs
@@ -1,5 +1,34 @@
-use ggez::nalgebra::Matrix4;
+use ggez::nalgebra::{Matrix4, Matrix, DMatrix, Vector};
+use rand::prelude::*;
+use itertools::Itertools;
+
+struct Layer(DMatrix<f64>);
 
 pub struct NN {
-    weights: Matrix4<f64>,
+    layers: Box<[Layer]>,
+}
+
+impl NN {
+    // layer_sizes[0] is input layer and layer_sizes[-1] is output layer
+    pub fn new(layer_sizes: &[usize]) -> Self {
+        let rng = &mut thread_rng();
+        let mut layers = vec![];
+        let mut windows = layer_sizes.windows(2);
+        while let Some(&[nrows, ncols]) = windows.next() {
+            let mat: DMatrix<f64> = DMatrix::from_fn(ncols, nrows, |_, _| rng.gen_range(0.0, 0.001));
+            layers.push(Layer(mat));
+        }
+        println!("len: {}", layers.len());
+        println!("dims: {:?}", layers.iter().map(|m| (m.0.ncols(), m.0.nrows())).collect::<Vec<_>>());
+        Self { layers: layers.into_boxed_slice() }
+    }
+
+    pub fn apply(&self, input: &[f64]) -> Box<[f64]> {
+        assert_eq!(input.len(), self.layers[0].0.ncols());
+        let mut vec = DMatrix::from_column_slice(input.len(), 1, input);
+        for Layer(weights) in self.layers.iter() {
+            vec = weights * vec;
+        }
+        vec.iter().copied().collect::<Vec<_>>().into_boxed_slice()
+    }
 }
